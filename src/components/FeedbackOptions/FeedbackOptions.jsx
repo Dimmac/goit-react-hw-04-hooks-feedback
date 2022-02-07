0156e7591b6af91d6d80881ee7575ccd67525ef9
Dimmac <dmytro.lojivsky@gmail.com>
2022-02-07 23:03:48 +0200

    initial

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { FeedbackButtonStyled } from './FeedbackOptions.styled';
+
+function FeedbackOptions(props) {
+  const { options, onLeaveFeedback } = props;
+  return options.map((option, index) => {
+    return (
+      <FeedbackButtonStyled
+        key={index}
+        type="button"
+        onClick={() => onLeaveFeedback(option)}
+      >
+        {option}
+      </FeedbackButtonStyled>
+    );
+  });
+}
+
+export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
