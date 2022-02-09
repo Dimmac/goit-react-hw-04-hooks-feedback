4650cfbf6bba8abc1491c09223812078b0763017
Dimmac <dmytro.lojivsky@gmail.com>
2022-02-09 23:53:10 +0200

    change construction

--- src/App.js
+++ src/App.js
@@ -10,23 +10,18 @@ const [good, setGood] = useState(0);
 const [neutral, setNeutral] = useState(0);
 const [bad, setBad] = useState(0);
 
-const handleIncrementFeedback = (position) => {
-  switch (position) {
-    case 'good': setGood((prevState) => prevState+1)
-    break
-
-    case 'neutral': setNeutral((prevState) => prevState+1)
-    break
-
-    case 'bad': setBad((prevState) => prevState+1)
-    break
-
+const handleIncrementFeedback = (value) => {
+  switch (value) {
+    case 'good': setGood(good+1)
+    break;
+    case 'neutral': setNeutral(neutral+1)
+    break;
+    case 'bad': setBad(bad+1)
+    break;
     default:
     console.log('Invalid data type')
   }
 }
-
-
 
 const countTotalFeedback = () => good + neutral + bad;
 const countPositiveFeedbackPercentage = () => {
