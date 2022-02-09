b543f18d2eefe8e8a0c5bd1af2ddec2e3cccfdac
Dimmac <dmytro.lojivsky@gmail.com>
2022-02-09 23:02:04 +0200

    manage hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
-import React from 'react';
+import {useState} from 'react';
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Statistics from './components/Statistics/Statistics';
 import Section from './components/Section/Section';
 import Notification from './components/Notification/Notification';
 import { SectionButtonStyled, StatisticTitle } from './App.styled';
 
-class Feedback extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  handleIncrementFeedback = position => {
-    this.setState(prevState => {
-      return { [position]: prevState[position] + 1 };
-    });
-  };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return good
-      ? `${Math.round((good * 100) / this.countTotalFeedback()) + '%'}`
-      : `0`;
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <Section title="Please leave feedback">
-        <SectionButtonStyled>
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleIncrementFeedback}
-          />
-        </SectionButtonStyled>
-        <StatisticTitle>Statistics</StatisticTitle>
-        {good === 0 && neutral === 0 && bad === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        )}
-      </Section>
-    );
+export default function Feedback() {
+const [good, setGood] = useState(0);
+const [neutral, setNeutral] = useState(0);
+const [bad, setBad] = useState(0);
+
+const handleIncrementFeedback = (position) => {
+  switch (position) {
+    case 'good': setGood((prevState) => prevState+1)
+    break
+
+    case 'neutral': setNeutral((prevState) => prevState+1)
+    break
+
+    case 'bad': setBad((prevState) => prevState+1)
+    break
+
+    default:
+    console.log('Invalid data type')
   }
 }
-export default Feedback;
+
+
+
+const countTotalFeedback = () => good + neutral + bad;
+const countPositiveFeedbackPercentage = () => {
+  return good
+    ? `${Math.round((good * 100) / countTotalFeedback()) + '%'}`
+    : `0`;
+};
+
+  return (<Section title="Please leave feedback">
+  <SectionButtonStyled>
+    <FeedbackOptions
+      options={['good', 'neutral', 'bad']}
+      onLeaveFeedback={handleIncrementFeedback}
+    />
+  </SectionButtonStyled>
+  <StatisticTitle>Statistics</StatisticTitle>
+  {good === 0 && neutral === 0 && bad === 0 ? (
+    <Notification message="There is no feedback" />
+  ) : (
+    <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      total={countTotalFeedback()}
+      positivePercentage={countPositiveFeedbackPercentage()}
+    />
+  )}
+</Section>)
+};
